7a89b9e24daed8549d535dec8c2eca8c63aaf66b
jiangxincan <jiangxincan@hatech.com.cn>
2019-11-06 17:25:18 +0800

    添加eureka配置，添加openfeign依赖

--- xincan-transaction-role/src/main/java/com/xincan/transaction/XincanTransactionRoleApplication.java
+++ xincan-transaction-role/src/main/java/com/xincan/transaction/XincanTransactionRoleApplication.java
@@ -2,9 +2,10 @@ package com.xincan.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-//@MapperScan("com.xincan.transaction.server.mapper.**.*")
 public class XincanTransactionRoleApplication {
 
     public static void main(String[] args) {
--- xincan-transaction-user/src/main/java/com/xincan/transaction/XincanTransactionUserApplication.java
+++ xincan-transaction-user/src/main/java/com/xincan/transaction/XincanTransactionUserApplication.java
@@ -3,9 +3,9 @@ package com.xincan.transaction;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@MapperScan("com.xincan.transaction.server.mapper.**.*")
 public class XincanTransactionUserApplication {
 
     public static void main(String[] args) {
