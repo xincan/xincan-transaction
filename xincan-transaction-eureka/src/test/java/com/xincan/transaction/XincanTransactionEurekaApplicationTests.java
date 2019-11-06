976164f7bb0e1268afd20cf5098fda44aec2bbba
jiangxincan <jiangxincan@hatech.com.cn>
2019-11-06 14:59:04 +0800

    优化eureka测试类

--- xincan-transaction-eureka/src/test/java/com/xincan/transaction/XincanTransactionEurekaApplicationTests.java
+++ xincan-transaction-eureka/src/test/java/com/xincan/transaction/XincanTransactionEurekaApplicationTests.java
@@ -4,7 +4,7 @@ package com.xincan.transaction;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = XincanTransactionEurekaApplication.class)
 class XincanTransactionEurekaApplicationTests {
 
     @Test
