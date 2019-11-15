ebbc741f8be2bbe2c53ee28d836798fa6fbe2d1a
jiangxincan <jiangxincan@hatech.com.cn>
2019-11-15 10:04:08 +0800

    初始化gateway项目

---
+++ xincan-transaction-gateway/src/main/java/com/xincan/transaction/XincanTransactionGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.xincan.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XincanTransactionGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XincanTransactionGatewayApplication.class, args);
+    }
+
+}
---
+++ xincan-transaction-gateway/src/test/java/com/xincan/transaction/XincanTransactionGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xincan.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XincanTransactionGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
